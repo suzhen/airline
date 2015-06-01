11430b95fc601b9da44b2454dbc6ba59034231cb
suezhen <sz3001@gmail.com>
2015-06-01 16:15:10 +0800

    deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,17 +1,26 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'airline'
+set :repo_url, 'https://github.com/suzhen/airline.git'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :branch,'master'
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/airline'
+
+set :bundle_flags, '--deployment'
+
+set :bundle_gemfile, -> { release_path.join('Gemfile') }
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+
+set :chruby_ruby, 'ruby-2.2.1'
+
+set :bundle_binstubs, nil
 
 # Default value for :format is :pretty
 # set :format, :pretty
@@ -23,26 +32,29 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :pty, true
 
 # Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml','config/boot_app.yml')
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+set :default_env, { path: "/opt/rubies/ruby-2.2.1/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
+  task :restart_thin  do
+    on roles(:web),in: :sequence, wait: 3 do
+      within release_path do
+        execute :bundle,"exec thin restart -C /var/www/airline/shared/config/boot_app.yml"
+      end
     end
+
   end
+
+
+  after :published, :restart_thin
 
 end
